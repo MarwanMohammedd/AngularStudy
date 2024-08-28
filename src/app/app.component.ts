a5f9e64c96ef4e91280829d2b20ddbb3dc0e27db
MarwanMohammed <marwan.m.rabea@gmail.com>
2024-08-28 14:55:21 +0300

    Add Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app-root', // HTML TAG
+  standalone: true, // StandAlone compoment not depend on any class , component
+  imports: [RouterOutlet], // this component is import class to use it 
+  templateUrl: './app.component.html', // component's HMTL File
+  styleUrl: './app.component.css' // Component's Style File or many files styleUrls take array of files name 
 })
-export class AppComponent {
+// name conventions in angular is app-COMPONENTNAME
+export class AppComponent { // this class that is decorated with @component 
+  helloWorld : string = "hello,World!" ;
   title = 'wishlist';
 }
+// for create a component use npm g{generate} c{component} NAMEOFCOMPONENT
